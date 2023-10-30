2fe2be2446b5f0e1b9474d6cedf4dfa3bc3fd0df
Huyen Pham <88421869+huyenphamkr@users.noreply.github.com>
2023-10-30 22:24:04 +0700

    Update note.php

--- note.php
+++ note.php
@@ -1,3 +1,4 @@
 <input type="hidden" value = "{{old('page', $paginator->currentPage())}}" name="page" id="page">
 $request->session()->put('key', 'value');
 
+https://chungnguyen.xyz/posts/code-laravel-lam-sao-cho-chuan
