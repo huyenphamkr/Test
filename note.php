fe5fd2c4c34d839f85c7b5bcaf5d2182b1d4cf9c
Huyen Pham <88421869+huyenphamkr@users.noreply.github.com>
2023-11-07 00:05:35 +0700

    Update note.php

--- note.php
+++ note.php
@@ -33,3 +33,63 @@
     }
 
     </script>
+
+/----------------------------------------------------------------------------------
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Định dạng số</title>
+</head>
+<body>
+    <input type="text" id="inputNumber" placeholder="Nhập số">
+    <button onclick="formatNumber()">Định dạng</button>
+    <p id="formattedNumber"></p>
+
+    <script>
+        function formatNumber() {
+            const inputElement = document.getElementById("inputNumber");
+            const inputValue = inputElement.value;
+
+            // Xóa tất cả các dấu chấm trong chuỗi
+            const numberString = inputValue.replace(/\./g, '');
+
+            // Chuyển đổi chuỗi thành số
+            const number = parseInt(numberString, 10);
+
+            // Định dạng số theo định dạng "9.999.999"
+            const formattedNumber = new Intl.NumberFormat().format(number);
+
+            // Hiển thị số đã định dạng
+            document.getElementById("formattedNumber").textContent = formattedNumber;
+        }
+    </script>
+</body>
+</html>
+---------------------------------------------------------------------------
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Định dạng và maxlength</title>
+</head>
+<body>
+    <input type="text" id="inputNumber" placeholder="Nhập số %" maxlength="2" oninput="formatAndLimitInput(this)">
+    
+    <script>
+        function formatAndLimitInput(inputElement) {
+            // Loại bỏ tất cả ký tự không phải số và ký tự %
+            inputElement.value = inputElement.value.replace(/[^\d%]/g, '');
+
+            // Giới hạn độ dài của văn bản
+            if (inputElement.value.length > 2) {
+                inputElement.value = inputElement.value.slice(0, 2);
+            }
+        }
+    </script>
+</body>
+</html>
+
+---------------------------------------------------------------------------
