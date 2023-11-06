7eb9a0740c34f78468444736c17504856430dd7f
Huyen Pham <88421869+huyenphamkr@users.noreply.github.com>
2023-11-06 22:49:23 +0700

    Update note.php

--- note.php
+++ note.php
@@ -1,4 +1,35 @@
-<input type="hidden" value = "{{old('page', $paginator->currentPage())}}" name="page" id="page">
-$request->session()->put('key', 'value');
+//input yên
+<div class="input-group">
+  <input type="text" class="form-control">
+  <span class="input-group-text">円</span>
+</div>
 
-https://chungnguyen.xyz/posts/code-laravel-lam-sao-cho-chuan
+
+//add
+ <div class="input-group">
+   <input type="text" class="form-control border-radius-none">
+   <button class="btn btn-outline-secondary border-radius-none" type="button">+</button>
+</div>
+
+//Hidden or Display
+<script>
+    var x = document.getElementById("cbb").value;
+    if (x) {
+        alert(x);
+        document.getElementById("txtDateS").disabled = false;
+    } else {
+        alert('ronf');
+        alert(x);
+        document.getElementById("txtDateS").disabled = true;
+    }
+    disableFunc();
+    function disableFunc() {
+        var e = document.getElementById("cbb").value;
+        if (e == 2) {
+            document.getElementById("txtDateS").disabled = true;
+        } else {
+            document.getElementById("txtDateS").disabled = false;
+        }
+    }
+
+    </script>
