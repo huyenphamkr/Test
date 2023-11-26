d502062cf9a86e0bac18ad9b8125c2bb963c72f6
Huyen Pham <88421869+huyenphamkr@users.noreply.github.com>
2023-11-26 20:13:26 +0700

    Update  Requests.php

---
+++ Requests.php
@@ -0,0 +1,102 @@
+<?php
+
+
+namespace App\Http\Requests;
+
+
+use App\Common\CommonConst;
+use Illuminate\Foundation\Http\FormRequest;
+
+
+class U03Request extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $validate = [];
+        if($this->has('txtTitle_invitation')){
+            $validate['txtTitle_invitation'] = 'required|max:30';
+            $validate['txtIntro_invitation'] = 'required|max:500';
+            $validate['txtBody_invitation'] = 'required|max:1000';
+        }
+
+        if($this->has('txtDate_join') && $this->has('txtLastName')){
+            if($this->has('txtLastName')){
+                // $validate['users_number'] = 'nullable|max:7|unique:info_users,users_number,' . $this->id . ',id,office_cd,'.$this->input('cmbOffice_cd');
+                $validate['txtNote'] = 'required|max:500';
+                $validate['txtDate_join'] = 'required';
+                $validate['txtFirstName'] = 'required|max:30';
+                $validate['txtLastName'] = 'required|max:30';
+                $validate['txtFirstFurigana'] = 'required|max:30';
+                $validate['txtLastFurigana'] = 'required|max:30';
+                $validate['cmbWorkStatus'] = 'required';
+                $validate['cmbOffice_cd'] = 'required';
+                $validate['cmbBelong_cd'] = 'required';
+                $validate['txtPosition'] = 'max:30';
+                $validate['cmbWork_time_flag'] = 'required';
+                $validate['users_number'] = 'nullable|max:7|unique:info_users,users_number,' . $this->id;
+                $validate['txtCompany_mobile_number'] = 'nullable|max:20';
+                // $validate['txtResume'] = 'required';
+                $validate['email'] = 'required|email|required_with:confirm_email|same:confirm_email|max:25';
+                $validate['confirm_email'] = 'required|email|max:25';
+                if($this->input('hidStatus') != CommonConst::STATUS_CREATOR && $this->input('hidStatus') != '')
+                {
+                    $validate['txtConditions'] = 'required';
+                    $validate['txtCollectionFees'] = 'required';
+                    $validate['txtContract'] = 'required';
+                }
+   
+            }
+        }
+        return $validate;
+    }
+   
+    public function messages()
+    {
+        return CommonConst::RULE_COMMON;
+    }
+
+    public function attributes()
+    {
+        return [
+            'txtTitle_invitation' => 'タイトル',
+            'txtIntro_invitation' => '前書き',
+            'txtBody_invitation' => '本文',
+
+            'txtNote' => 'コメント',
+            'txtDate_join' => '入社予定日',
+            'txtFirstName' => '氏名（姓）',
+            'txtLastName' => '氏名（名）',
+            'txtFirstFurigana' => 'フリガナ（姓）',
+            'txtLastFurigana' => 'フリガナ（名）',
+            'cmbWorkStatus' => 'ステータス',
+            'cmbOffice_cd' => '事業所cd',
+            'cmbBelong_cd' => '所属cd',
+            'txtPosition' => '役職',
+            'cmbWork_time_flag' => '正社員・パート',
+            'users_number' => '社員番号',
+            'txtCompany_mobile_number' => '会社携帯番号',
+            'txtResume' => '履歴書',
+            'email' => 'メールアドレス',
+            'confirm_email' => 'メールアドレス_再確認',
+            'txtConditions' => '外国人雇用）雇用条件書',
+            'txtCollectionFees' => '（外国人雇用）徴収費用の説明書',
+            'txtContract' => '（外国人雇用）雇用契約書',
+        ];
+    }
+}
