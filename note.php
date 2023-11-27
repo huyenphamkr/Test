8555b075df9e4928af95e1043132d6953d1b070c
Huyen Pham <88421869+huyenphamkr@users.noreply.github.com>
2023-11-27 23:33:43 +0700

    migration

--- note.php
+++ note.php
@@ -1,56 +1,69 @@
------------------------------------------đệm 0 trước---------------------------------------------
-function padZeroToSeven($input) {
-    return str_pad($input, 7, '0', STR_PAD_LEFT);
-}
+<?php
+use IlluminateDatabaseMigrationsMigration;
+use IlluminateDatabaseSchemaBlueprint;
+use IlluminateSupportFacadesSchema;
 
-// Sử dụng function: đệm 0 trước
-$inputNumber = "12345";
-$result = padZeroToSeven($inputNumber);
-echo "Kết quả: " . $result;
------------------------------------------xóa đệm 0 trước---------------------------------------------
-function padZeroToSeven($input) {
-    $paddedInput = str_pad($input, 7, '0', STR_PAD_LEFT);
-    return ltrim($paddedInput, '0');
-}
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('info_users_ver_his', function (Blueprint $table) {
+                $table->id()->comment('入職者情報歴史ID');
+                $table->bigInteger('info_users_cd')->comment("入職者情報cd");
+                $table->integer('ver_his')->nullable()->comment("リビジョン歴史");
+                $table->string('first_name' ,60)->nullable()->comment("氏名（姓）");
+                $table->string('last_name' ,60)->nullable()->comment("氏名（名）");
+                $table->string('first_furigana' ,60)->nullable()->comment("フリガナ（姓）");
+                $table->string('last_furigana' ,60)->nullable()->comment("フリガナ（名）");
+                $table->string('contract_class' ,20)->nullable()->comment("契約区分 : 【MST_CLASS】.「item_type」= 'CONTRACT_CLASS'");
+                $table->string('gender' ,20)->nullable()->comment("性別 : 【MST_CLASS】.「item_type」= 'GENDER'");
+                $table->string('tel_number' ,20)->nullable()->comment("電話番号");
+                $table->string('post_code' ,7)->nullable()->comment("郵便番号");
+                $table->string('adress1' ,40)->nullable()->comment("住所1");
+                $table->string('adress2' ,40)->nullable()->comment("住所2");
+                $table->string('adress3' ,40)->nullable()->comment("住所3");
+                $table->string('adress_furigana1' ,40)->nullable()->comment("住所1（フリガナ）");
+                $table->string('adress_furigana2' ,40)->nullable()->comment("住所2（フリガナ）");
+                $table->string('adress_furigana3' ,40)->nullable()->comment("住所3（フリガナ）");
+                $table->timestamp('birthday')->nullable()->comment("生年月日");
+                $table->string('prev_job' ,100)->nullable()->comment("前職（会社名）");
+                $table->string('my_number' ,12)->nullable()->comment("マイナンバー");
+                $table->string('object_person' ,20)->nullable()->comment("該当者 : 【MST_CLASS】.「item_type」= 'OBJECT_PERSON'");
+                $table->string('jion_insurance' ,20)->nullable()->comment("社会保険加入 : 【MST_CLASS】.「item_type」= 'YES_NO'");
+                $table->timestamp('insurance_date')->nullable()->comment("社会保険加入日");
+                $table->string('insurance_social' ,20)->nullable()->comment("雇用保険加入 : 【MST_CLASS】.「item_type」= 'YES_NO'");
+                $table->timestamp('insurance_social_date')->nullable()->comment("雇用保険加入日");
+                $table->integer('dependent_number')->nullable()->comment("扶養家族");
+                $table->string('bank_code' ,4)->nullable()->comment("銀行コード");
+                $table->string('branch_bank_code' ,3)->nullable()->comment("支店コード");
+                $table->string('bank_name' ,40)->nullable()->comment("銀行名・振興金庫名・組合名");
+                $table->string('branch_bank_name' ,40)->nullable()->comment("支店名・出張所/代理店名");
+                $table->string('branch_bank_number' ,6)->nullable()->comment("支店番号");
+                $table->string('bank_number' ,14)->nullable()->comment("口座番号");
+                $table->string('bank_user_name' ,60)->nullable()->comment("口座氏名");
+                $table->string('bank_user_furigana' ,60)->nullable()->comment("口座フリガナ");
+                $table->integer('private_car')->nullable()->comment("自家用車");
+                $table->string('transport_type' ,20)->nullable()->comment("その他 : 【MST_CLASS】.「item_type」= 'TRANSPORT_TYPE'");
+                $table->timestamp('created_at')->nullable()->useCurrent()->comment("登録日時");
+                $table->integer('created_id')->nullable()->comment("登録ユーザー");
+                $table->timestamp('updated_at')->nullable()->useCurrent()->useCurrentOnUpdate()->comment("更新日時");
+                $table->integer('updated_id')->nullable()->comment("更新ユーザー");
+                $table->integer('revision')->nullable()->comment("リビジョン番号");
+                
+    });
 
-// Sử dụng function:
-$inputNumber = "0012345";
-$result = padZeroToSeven($inputNumber);
-echo "Kết quả: " . $result;
------------------------------------------xác đinh check bõ đc chọn---------------------------------------------
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Checkbox Example</title>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-</head>
-<body>
-    <h2>Chọn các mục:</h2>
-    <input type="checkbox" class="myCheckbox" value="item1"> Mục 1<br>
-    <input type="checkbox" class="myCheckbox" value="item2"> Mục 2<br>
-    <input type="checkbox" class="myCheckbox" value="item3"> Mục 3<br>
-    <input type="checkbox" class="myCheckbox" value="item4"> Mục 4<br>
-    <input type="checkbox" class="myCheckbox" value="item5"> Mục 5<br>
-    <button id="checkButton">Kiểm tra</button>
-
-    <script>
-        $(document).ready(function(){
-            $('#checkButton').on('click', function(){
-                var atLeastOneChecked = false;
-                $('.myCheckbox').each(function(){
-                    if ($(this).is(':checked')) {
-                        atLeastOneChecked = true;
-                        return false; // Thoát khỏi vòng lặp khi tìm thấy checkbox được chọn
-                    }
-                });
-
-                if (atLeastOneChecked) {
-                    alert('Ít nhất một checkbox đã được chọn.');
-                } else {
-                    alert('Không có checkbox nào được chọn.');
-                }
-            });
-        });
-    </script>
-</body>
-</html>
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('info_users_ver_his');
+    }
+};
