852fa1d13d8c8eab8269138cc835e79fb46001c4
huyenphamkr <huyenjkx@gmail.com>
2023-12-04 22:14:35 +0700

    exportExxcel

---
+++ Export.php
@@ -0,0 +1,15 @@
+extension=gd trong php.ini
+run composer update
+run composer require maatwebsite/excel --with-all-dependencies
+
+
+
+
+
+https://makitweb.com/export-data-to-csv-excel-format-in-laravel-8/
+
+jp
+https://github.com/SpartnerNL/Laravel-Excel/issues/1171
+
+laravel excel
+https://docs.laravel-excel.com/nova/1.x/exports/customizations.html
